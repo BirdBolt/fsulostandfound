37f691d9699dbfeec2c7003da2626eea46cc9bac
BirdBolt <Njundongw@yahoo.com>
2017-04-19 06:54:31 -0400

    Added browse, modal, fixed buggy resizing menu bar
    Used media queries to properly resize nav bar on varies screen sizes and
    created a modal template for the browse section. Adjusted text alignment
    and began experimenting with php to dynamically display listings on
    browse modal

--- index.php
+++ index.php
@@ -145,27 +145,12 @@ function init() {
     <div class="modal-body">
        <div class="modal-content">
           <div class="">
-           <?php
-            $files = glob("images/*.*");
-             for ($i=1; $i<count($files); $i++)
-              {
-                $image = $files[$i];
-                $supported_file = array(
-                        'gif',
-                        'jpg',
-                        'jpeg',
-                        'png'
-                 );
-
-                 $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
-                 if (in_array($ext, $supported_file)) {
-                     echo $image ."<br />";
-                     echo '<img src="'.$image .'" alt="Random image" />'."<br /><br />";
-                    } else {
-                        continue;
-                    }
-                  }
-           ?>
+          <?php
+            echo "<h2>PHP is Fun!</h2>";
+            echo "Hello world!<br>";
+            echo "I'm about to learn PHP!<br>";
+            echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+            ?>
         </div>
 
         </div>
